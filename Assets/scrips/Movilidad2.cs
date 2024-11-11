2a0322f3e5e2a58ad043c05e272bbcafbd98af5e
gery24 <150917721+gery24@users.noreply.github.com>
2024-11-11 20:15:17 +0100

    escena casa dentro

---
+++ Assets/scrips/Movilidad2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movilidad2 : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private float _vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 8f;
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovimientoTorrente();
+    }
+
+    
+
+    public void MovimientoTorrente()
+    {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal") * _vel;
+        float inputVertical = Input.GetAxisRaw("Vertical") * _vel;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(inputHorizontal, inputVertical);
+    }
+}
