f6a691b71323df490668ecf50c420a82423da37a
gery24 <150917721+gery24@users.noreply.github.com>
2024-11-25 17:17:14 +0100

    Sala2 Terminada
    Se a añadido que todas las puertas funcionencionando, texto de la salida y las colisiones

--- Assets/scrips/MovimientoT.cs
+++ Assets/scrips/MovimientoT.cs
@@ -1,131 +1,108 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
-using UnityEngine.UI;
-using TMPro.Examples;
+    using JetBrains.Annotations;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Unity.VisualScripting;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
+    using TMPro;
 
+    public class MovimientoT : MonoBehaviour
+    {
+        private new Rigidbody2D rigidbody;
+        private float _vel;
+        Vector2 minPantalla, maxPantalla;
+        private Vector3 velocity;
+        public float direccioIndicadaX;
+        public float direccioIndicadaY;
+        public GameObject porta1;
+        public GameObject salida;
+        private bool estaEnP1 = false;
+        private bool estaEnP2 = false;
+        private bool estaEnP3 = false;
+        private bool estaEnP4 = false;
+        public GameObject paredMid;
+        public GameObject puertaCof;
+        public GameObject textPortaCasa;
+        public int vidasT;
 
+        //--------------------------------------------
+        public GameObject panelOperacion;
+        public TextMeshPro operacionText;
+        public GameObject P1;
+        public GameObject P2;
+        public GameObject P3;
+        public GameObject P4;
+        private int numero1, numero2, resultadoCorrecto;
+        private bool panelActivo = false; // Bandera para saber si el panel está activo
 
-public class MovimientoT : MonoBehaviour
-{
-    private new Rigidbody2D rigidbody;
-    private float _vel;
-    Vector2 minPantalla, maxPantalla;
-    private Vector3 velocity;
-    public float direccioIndicadaX;
-    public float direccioIndicadaY;
-    public GameObject porta1;
-    //public GameObject cofre;
-    public GameObject salida;
-    public bool textOperacion = false;
-    private bool estaEnP1 = false;
-
-    //--------------------------------------------
-    public GameObject panelOperacion;
-    public TextMeshPro operacionText;
-    //public InputField respuestaInput;
-    public GameObject P1;
-    private int numero1, numero2, resultadoCorrecto;
-    //public TextMeshPro Respuesta;
-
-    private bool panelActivo = false; // Bandera para saber si el panel está activo
-
-    [SerializeField] private Button BotonValidar; // Botón de confirmación
-    [SerializeField] private TMP_InputField Respuesta;
-
-
+        [SerializeField] private Button BotonValidar; // Botón de confirmación
+        [SerializeField] private TMP_InputField Respuesta;
+        [SerializeField] private TMPro.TextMeshProUGUI componentTextVides;
 
     // Start is called before the first frame update
     void Start()
-    {
-
-        _vel = 8f;
-        //minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        //maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-
-        //float meitatMidaImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
-        //float meitatMidaImatgeY = GetComponent<SpriteRenderer>().sprite.bounds.size.y * transform.localScale.y / 2;
-
-        //minPantalla.x = minPantalla.x + meitatMidaImatgeX;
-        //maxPantalla.x = maxPantalla.x - meitatMidaImatgeX;
-
-        rigidbody = GetComponent<Rigidbody2D>();
-
-        panelOperacion.SetActive(false);
-
-        if (VariablesGlobales.escenaAnterior == "cofre")
         {
-            transform.position = VariablesGlobales.posTorrenteGuardada;
+            _vel = 8f;
+            rigidbody = GetComponent<Rigidbody2D>();
+            panelOperacion.SetActive(false);
+
+            if (VariablesGlobales.escenaAnterior == "cofre")
+            {
+                transform.position = VariablesGlobales.posTorrenteGuardada;
+            }
+            vidasT = 3;
         }
 
-        //BotonValidar.onClick.AddListener(VerificarRespuesta);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        moverTorrente();
-        colisionTorrente();
-
-        // Solo genera operación si está en P1, se presiona "E", y el panel no está activo
-        if (estaEnP1 && Input.GetKeyDown(KeyCode.E) && !panelActivo)
+        // Update is called once per frame
+        void Update()
         {
-            Debug.Log("Generando operación...");
-            GenerarOperacion();
+            moverTorrente();
+            colisionTorrente();
+
+            if ((estaEnP1 || estaEnP2 || estaEnP3 || estaEnP4) && Input.GetKeyDown(KeyCode.E) && !panelActivo)
+            {
+                GenerarOperacion();
+            }
         }
 
-        // Cierra el panel si el jugador sale de P1 y el panel está activo
-        if (!estaEnP1 && panelActivo)
+        //GENERAR OPERACION
+        public void GenerarOperacion()
         {
-            CerrarPanelOperacion();
+            int num1 = Random.Range(1, 10);
+            int num2 = Random.Range(1, 10);
+            int operador = Random.Range(0, 4); // 0: suma, 1: resta, 2: multiplicación, 3: división
+            string operacionTexto = "";
+
+            switch (operador)
+            {
+                case 0: // Suma
+                    resultadoCorrecto = num1 + num2;
+                    operacionTexto = num1 + " + " + num2;
+                    break;
+
+                case 1: // Resta
+                    resultadoCorrecto = num1 - num2;
+                    operacionTexto = num1 + " - " + num2;
+                    break;
+
+                case 2: // Multiplicación
+                    resultadoCorrecto = num1 * num2;
+                    operacionTexto = num1 + " * " + num2;
+                    break;
+
+                case 3: // División (asegura que el resultado sea entero)
+                    num1 = num1 * num2; // Así el primer número será divisible por el segundo
+                    resultadoCorrecto = num1 / num2;
+                    operacionTexto = num1 + " / " + num2;
+                    break;
+            }
+
+            operacionText.text = operacionTexto + " = ";
+            Respuesta.text = ""; // Limpia el campo de entrada
+            panelOperacion.SetActive(true);
+            panelActivo = true; // Marca el panel como activo
         }
-    }
-
-    //GENERAR OPERACION
-    public void GenerarOperacion()
-    {
-        int num1 = Random.Range(1, 10);
-        int num2 = Random.Range(1, 10);
-        int operador = Random.Range(0, 4); // 0: suma, 1: resta, 2: multiplicación, 3: división
-        string operacionTexto = "";
-
-        switch (operador)
-        {
-            case 0: // Suma
-                resultadoCorrecto = num1 + num2;
-                operacionTexto = num1 + " + " + num2;
-                break;
-
-            case 1: // Resta
-                resultadoCorrecto = num1 - num2;
-                operacionTexto = num1 + " - " + num2;
-                break;
-
-            case 2: // Multiplicación
-                resultadoCorrecto = num1 * num2;
-                operacionTexto = num1 + " * " + num2;
-                break;
-
-            case 3: // División (asegura que el resultado sea entero)
-                num1 = num1 * num2; // Así el primer número será divisible por el segundo
-                resultadoCorrecto = num1 / num2;
-                operacionTexto = num1 + " / " + num2;
-                break;
-        }
-
-        // Muestra la operación en el texto y limpia el campo de respuesta
-        operacionText.text = operacionTexto + " = ";
-        Respuesta.text = ""; // Limpia el campo de entrada
-        panelOperacion.SetActive(true);
-        panelActivo = true; // Marca el panel como activo
-    }
 
     // Verifica la respuesta del jugador
     public void VerificarRespuesta()
@@ -136,12 +113,23 @@ public class MovimientoT : MonoBehaviour
             if (respuestaJugador == resultadoCorrecto)
             {
                 Debug.Log("¡Respuesta correcta!");
-                panelOperacion.SetActive(false); 
+                panelOperacion.SetActive(false);
+                panelActivo = false;
+
+                if (estaEnP1) { P1.SetActive(false); estaEnP1 = false; }
+                if (estaEnP2) { P2.SetActive(false); paredMid.SetActive(false); estaEnP2 = false; }
+                if (estaEnP3) { P3.SetActive(false); puertaCof.SetActive(false); estaEnP3 = false; }
+                if (estaEnP4) { P4.SetActive(false); estaEnP4 = false; }
             }
             else
             {
                 Debug.Log("Respuesta incorrecta. Intenta de nuevo.");
-                GenerarOperacion(); 
+                vidasT--;
+                componentTextVides.text = "Vides: " + vidasT.ToString();
+                if (vidasT <= 0)
+                {
+                    SalirDelJuego();
+                }
             }
         }
         else
@@ -150,69 +138,94 @@ public class MovimientoT : MonoBehaviour
         }
     }
 
-    // Cierra el panel de operación
-    public void CerrarPanelOperacion()
-    {
-        panelOperacion.SetActive(false);
-        panelActivo = false; // Marca el panel como inactivo
-    }
 
     //MOVIMIENTO TORRENTE
     public void moverTorrente()
-    {
-        direccioIndicadaX = Input.GetAxisRaw("Horizontal");
-        direccioIndicadaY = Input.GetAxisRaw("Vertical");
-        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
-    }
-
-    //COLISIONES TORRENTE
-    public void colisionTorrente()
-    {
-        float inputHorizontal = Input.GetAxisRaw("Horizontal") * _vel;
-        float inputVertical = Input.GetAxisRaw("Vertical") * _vel;
-
-        Debug.Log("hola");
-
-        rigidbody.velocity = new Vector2(inputHorizontal, inputVertical);
-    }
-
-    //COLISION TEXTO PUERTA
-    public void OnCollisionEnter2D(Collision2D objecteTocat)
-    {
-        if (objecteTocat.gameObject.tag == "Porta")
         {
-            porta1.SetActive(true);
-            Invoke("DesactivarPorta1", 2f);
-
-            estaEnP1 = true; // El jugador está tocando P1
+            direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+            direccioIndicadaY = Input.GetAxisRaw("Vertical");
+            Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
         }
-        if (objecteTocat.gameObject.tag == "sala")
+
+        //COLISIONES TORRENTE
+        public void colisionTorrente()
         {
-            VariablesGlobales.posTorrenteGuardada = transform.position;
-            if (VariablesGlobales.escenaAnterior != "cofre")
+            float inputHorizontal = Input.GetAxisRaw("Horizontal") * _vel;
+            float inputVertical = Input.GetAxisRaw("Vertical") * _vel;
+
+            rigidbody.velocity = new Vector2(inputHorizontal, inputVertical);
+        }
+
+        //COLISION TEXTO PUERTA
+        public void OnCollisionEnter2D(Collision2D objecteTocat)
+        {
+            if (objecteTocat.gameObject.tag == "Porta")
             {
-                SceneManager.LoadScene("cofre");
+                porta1.SetActive(true);
+                Invoke("DesactivarPorta1", 2f);
+                estaEnP1 = true;
+            }
+            if (objecteTocat.gameObject.tag == "Porta2")
+            {
+                porta1.SetActive(true);
+                Invoke("DesactivarPorta1", 2f);
+                estaEnP2 = true;
+            }
+            if (objecteTocat.gameObject.tag == "Porta3")
+            {
+                porta1.SetActive(true);
+                Invoke("DesactivarPorta1", 2f);
+                estaEnP3 = true;
+            }
+            if (objecteTocat.gameObject.tag == "Porta4")
+            {
+                porta1.SetActive(true);
+                Invoke("DesactivarPorta1", 2f);
+                estaEnP4 = true;
+            }
+            if (objecteTocat.gameObject.tag == "salida")
+            {
+                textPortaCasa.SetActive(true);
+                Invoke("interactuar2", 2f);
+            }
+    }
+
+        private void OnCollisionExit2D(Collision2D objecteTocat)
+        {
+            if (objecteTocat.gameObject.tag == "Porta")
+            {
+                estaEnP1 = false;
+            }
+            if (objecteTocat.gameObject.tag == "Porta2")
+            {
+                estaEnP2 = false;
+            }
+            if (objecteTocat.gameObject.tag == "Porta3")
+            {
+                estaEnP3 = false;
+            }
+            if (objecteTocat.gameObject.tag == "Porta4")
+            {
+                estaEnP4 = false;
             }
         }
-        if (objecteTocat.gameObject.tag == "salida")
+
+        //DESACTIVAR PUERTA
+        public void DesactivarPorta1()
         {
-            SceneManager.LoadScene("casa");
+            porta1.SetActive(false);
         }
-    }
-
-    private void OnCollisionExit2D(Collision2D objecteTocat)
-    {
-        if (objecteTocat.gameObject.tag == "Porta")
+        public void SalirDelJuego()
         {
-            Debug.Log("Saliendo de P1...");
-
-            estaEnP1 = false; // El jugador ha salido de P1
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
         }
-    }
-
-    //DESACTIVAR PUERTA
-    public void DesactivarPorta1()
-    {
-        porta1.SetActive(false);
-    }
+        
+        public void interactuar2()
+        {
+            textPortaCasa.SetActive(false);
+        }
 }
